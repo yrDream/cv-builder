d52f5d2dd035a98dc9b83f3d109aa06aa7a5aeef
yrDream <aneron334@gmail.com>
2022-04-11 21:28:44 +0300

    master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from "react"
 
 const App = () => {
 
-    return <p>Hello World</p>;
+    return(
+        <div className="ui-wrapper">
+
+      <header>header</header>
+
+      <div className="ui-content-wrapper">
+
+        <section className="ui-section">
+          <div className="ui-container">
+            <h1 className="ui-title-1">Hello world!</h1>
+          </div>
+        </section>
+      </div>
+
+      <footer>footer</footer>
+    </div>
+    )
     
 };
 
