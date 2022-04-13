cd36db7454562d86332f05b1a4172d3ee96fab68
yrDream <aneron334@gmail.com>
2022-04-13 22:44:45 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react"
 
-import Header from './components/Header/Header'
-import Footer from "./components/Footer/Footer";
+
+import {Header, Footer, Avatar} from "./components";
+
 
 const App = () => {
 
+
+  const handleAvatarClick = () => {
+    console.log('avatar click')
+  }
+
+  const handlePrintClick = () => {
+    console.log('print click') 
+  }
+
     return( 
         <div className="ui-wrapper">
-
-      <Header />
-
+      <Header onClick={handlePrintClick} />
       <div className="ui-content-wrapper">
-
         <section className="ui-section">
           <div className="ui-container">
+            <Avatar onClick={handleAvatarClick} />
             <h1 className="ui-title-1">Hello world!</h1>
           </div>
         </section>
